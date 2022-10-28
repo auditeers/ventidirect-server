41ad5656316b597f2ef339803fd5166928c55bce
Maurice Lourens <maurice@auditeers.net>
2022-10-28 12:38:06 +0200

    models base

---
+++ database/migrations/2022_10_28_102100_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('short_description');
+            $table->string('description');
+            $table->string('ean', 50);
+            $table->foreignId('category_id')->constrained();
+            $table->decimal('price');
+            $table->unsignedBigInteger('stock');
+            $table->boolean('visible');
+            $table->timestamps();
+        });
+
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained();
+            $table->string('product_name');
+            $table->unsignedBigInteger('quantity');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

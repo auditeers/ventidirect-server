a4bbf1e314562b284bddf6d00c0c3679e2d0474e
Maurice Lourens <maurice@auditeers.net>
2022-11-01 07:43:16 +0100

    no message

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -93,7 +93,7 @@ return [
     'breadcrumbs' => false,
 
     // Horizontal navbar classes. Helps make the admin panel look similar to your project's design.
-    'header_class' => 'app-header bg-primary border-0 navbar',
+    'header_class' => 'app-header bg-primary border-0 navbar navbar-dark',
     // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
     // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
 

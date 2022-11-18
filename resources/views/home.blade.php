60fceb73a25dd10d860c21de2cc758939ac10863
Maurice Lourens <maurice@auditeers.net>
2022-11-18 13:24:36 +0100

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1163,12 +1163,12 @@
 
 
     <!-- Optional JavaScript; choose one of the two! -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.nice-select.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/popper.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/jquery.nice-select.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 
 </html>

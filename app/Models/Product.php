9a53cf90355c3433b87e5126c00f4bc4ebf38fe3
Maurice Lourens <maurice@auditeers.net>
2022-11-28 15:29:05 +0100

    laravel scout implementation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Support\Str;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Product extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $guarded = [];
 

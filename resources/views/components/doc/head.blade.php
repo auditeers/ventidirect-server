081c7877feac3bf595b92b28ad8d1446a81cbc43
Maurice Lourens <maurice@auditeers.net>
2022-11-28 16:48:39 +0100

    relative css paths

--- resources/views/components/doc/head.blade.php
+++ resources/views/components/doc/head.blade.php
@@ -4,16 +4,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/all.min.css" rel="stylesheet" />
-    <link href="assets/css/fontawesome.css" rel="stylesheet" />
-    <link href="assets/css/owl.carousel.min.css" rel="stylesheet" />
-    <link href="assets/css/nice-select.css" rel="stylesheet" />
-    <link href="assets/css/default.css" rel="stylesheet" />
-    <link href="assets/fonts/stylesheet.css" rel="stylesheet" />
-    <link href="assets/fonts/subfont/stylesheet.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <link href="assets/css/responsive.css" rel="stylesheet" />
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/all.min.css" rel="stylesheet" />
+    <link href="/assets/css/fontawesome.css" rel="stylesheet" />
+    <link href="/assets/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="/assets/css/nice-select.css" rel="stylesheet" />
+    <link href="/assets/css/default.css" rel="stylesheet" />
+    <link href="/assets/fonts/stylesheet.css" rel="stylesheet" />
+    <link href="/assets/fonts/subfont/stylesheet.css" rel="stylesheet" />
+    <link href="/assets/css/style.css" rel="stylesheet" />
+    <link href="/assets/css/responsive.css" rel="stylesheet" />
 
 
     <title>VentiDirect, Alles voor binnenklimaat</title>

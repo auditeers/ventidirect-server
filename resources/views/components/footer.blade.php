ef3985dff3668bf0f4b1e797e96fed579862ddc9
Maurice Lourens <maurice@auditeers.net>
2022-11-28 15:08:11 +0100

    footer to component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,271 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+<!-- =================== FOOTER TOP AREA START ===================== -->
+    <section class="footer__top__area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 d-md-none d-lg-block">
+                    <div class="footer__top__profile">
+                        <div class="footer__top__profile__thumb">
+                            <img src="/assets/img/profile_1.png" alt="">
+                        </div>
+                        <div class="profile__content">
+                            <h5>Hulp nodig?</h5>
+                            <p>We helpen je graag verder</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <a href="tell:310800534515">
+                        <div class="footer__top__contact">
+                            <div class="footer__top__contact__ico">
+                                <img src="/assets/img/phone.png" alt="">
+                            </div>
+                            <div class="profile__content">
+                                <h5>+31 0800 534515</h5>
+                                <p>Bel van 08:00 tot 17:00</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <a href="#">
+                        <div class="footer__top__contact">
+                            <div class="footer__top__contact__ico">
+                                <img src="/assets/img/massage.png" alt="">
+                            </div>
+                            <div class="profile__content">
+                                <h5>Mail ons</h5>
+                                <p>Antwoord binnen 1 dag</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <a href="#">
+                        <div class="footer__top__contact">
+                            <div class="footer__top__contact__ico">
+                                <img src="/assets/img/badges.png" alt="">
+                            </div>
+                            <div class="profile__content">
+                                <h5>Instructies</h5>
+                                <p>Direct antwoord</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- =================== FOOTER TOP AREA END ===================== -->
+
+
+    <!-- =================== FOORTER AREA START ===================== -->
+    <footer class="footer__area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="footer__widgeta">
+                        <div class="footer__content">
+                            <h5>Klantenservice</h5>
+                            <div class="footer__list">
+                                <ul>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="#">Track & Trace order</a></li>
+                                    <li><a href="#">Bezorgen en afhalen</a></li>
+                                    <li><a href="#">Same day delivery</a></li>
+                                    <li><a href="#">Ruilen en retourneren</a></li>
+                                    <li><a href="#">Veelgestelde vragen</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer__widgeta">
+                        <div class="footer__content">
+                            <h5>VentiDirect</h5>
+                            <div class="footer__list">
+                                <ul>
+                                    <li><a href="#">Mijn account</a></li>
+                                    <li><a href="#">Over VentiDirect</a></li>
+                                    <li><a href="#">jyiuBlogtiut</a></li>
+                                    <li><a href="#">Zakelijke klant worden</a></li>
+                                    <li><a href="#">Vacatures</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer__widgeta">
+                        <div class="footer__content">
+                            <h5>Snel aan de slag</h5>
+                            <div class="footer__list">
+                                <ul>
+                                    <li><a href="#">Top 10 ventilatoren</a></li>
+                                    <li><a href="#">Bestsellers</a></li>
+                                    <li><a href="#">Alle categorieen</a></li>
+                                    <li><a href="#">Design roosters</a></li>
+                                    <li><a href="#">Ventilatie aansluiten</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer__widgeta">
+                        <div class="footer__content">
+                            <h5>Social Media</h5>
+                            <div class="footer__list">
+                                <ul>
+                                    <li><a href="#"><i class="fab fa-facebook"></i> Volg ons op Facebook</a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i> Volg ons op Twitter</a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i> Volg ons op LinkedIn</a></li>
+                                    <li><a href="#"><i class="fab fa-pinterest"></i> Volg ons op Pinterest</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="footer__accordion__blk d__none__accordion">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Klantenservice
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="footer__list">
+                                    <ul>
+                                        <li><a href="#">Contact</a></li>
+                                        <li><a href="#">Track &amp; Trace order</a></li>
+                                        <li><a href="#">Bezorgen en afhalen</a></li>
+                                        <li><a href="#">Same day delivery</a></li>
+                                        <li><a href="#">Ruilen en retourneren</a></li>
+                                        <li><a href="#">Veelgestelde vragen</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                VentiDirect
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="footer__list">
+                                    <ul>
+                                        <li><a href="#">Mijn account</a></li>
+                                        <li><a href="#">Over VentiDirect</a></li>
+                                        <li><a href="#">jyiuBlogtiut</a></li>
+                                        <li><a href="#">Zakelijke klant worden</a></li>
+                                        <li><a href="#">Vacatures</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Snel aan de slag
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="footer__list">
+                                    <ul>
+                                        <li><a href="#">Top 10 ventilatoren</a></li>
+                                        <li><a href="#">Bestsellers</a></li>
+                                        <li><a href="#">Alle categorieen</a></li>
+                                        <li><a href="#">Design roosters</a></li>
+                                        <li><a href="#">Ventilatie aansluiten</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                Social Media
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="footer__list">
+                                    <ul>
+                                        <li><a href="#"><i class="fab fa-facebook"></i> Volg ons op Facebook</a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i> Volg ons op Twitter</a></li>
+                                        <li><a href="#"><i class="fab fa-linkedin-in"></i> Volg ons op LinkedIn</a></li>
+                                        <li><a href="#"><i class="fab fa-pinterest"></i> Volg ons op Pinterest</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- =================== FOORTER AREA END ===================== -->
+
+
+    <!-- =================== FOORTER BOTTOM AREA START ===================== -->
+    <section class="footer__bottom__area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer__bottom__inner__blk">
+                        <div class="footer__payment">
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_1.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_2.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_3.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_4.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_5.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_6.png" alt=""></a>
+                            </div>
+                            <div class="single__payment__blk">
+                                <a href="#"><img src="/assets/img/pay_7.png" alt=""></a>
+                            </div>
+                        </div>
+
+                        <div class="footer__right__blk">
+                            <div class="footer__link">
+                                <a href="#">Algemene voorwaarden</a>
+                                <a href="#">Privacy</a>
+                                <a href="#">Cookies</a>
+                                <a href="#">Sitemap</a>
+                                <p>© Opgericht in 2022 - VentiDirect B.V.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- =================== FOORTER BOTTOM AREA END ===================== -->

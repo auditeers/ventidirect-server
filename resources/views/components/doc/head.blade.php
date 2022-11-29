416cb861dcb7a8b6601c5644916c2b2d3dff12ec
Maurice Lourens <maurice@auditeers.net>
2022-11-29 18:46:40 +0100

    favicons

--- resources/views/components/doc/head.blade.php
+++ resources/views/components/doc/head.blade.php
@@ -15,6 +15,15 @@
     <link href="/assets/css/style.css" rel="stylesheet" />
     <link href="/assets/css/responsive.css" rel="stylesheet" />
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+    <link rel="manifest" href="/icons/site.webmanifest">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#0022d1">
+    <link rel="shortcut icon" href="/icons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#0022d1">
+    <meta name="msapplication-config" content="/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
     <title>VentiDirect, Alles voor binnenklimaat</title>
 </head>

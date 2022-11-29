e7d0b5e26f17410bb2bc9630f76fdf78f67b00b7
Maurice Lourens <maurice@auditeers.net>
2022-11-30 00:54:22 +0100

    adding aos

--- resources/views/components/doc/script.blade.php
+++ resources/views/components/doc/script.blade.php
@@ -2,8 +2,18 @@
 <script src="/assets/js/popper.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
 <script src="/assets/js/owl.carousel.min.js"></script>
+
+<script src="/assets/js/aos.js"></script>
 <script src="/assets/js/slick.min.js"></script>
 <script src="/assets/js/jquery.nice-select.js"></script>
-
 <script src="/assets/js/main.js"></script>
 
+
+<!-- Optional JavaScript; choose one of the two! -->
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/popper.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    
+    <script src="/assets/js/jquery.nice-select.js"></script>
+    <script src="/assets/js/main.js"></script>

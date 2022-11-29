4df8e4f6a7f39a8c7a558aaf5717d2472cd47ccd
Maurice Lourens <maurice@auditeers.net>
2022-11-30 00:55:50 +0100

    doubles?

--- resources/views/components/doc/script.blade.php
+++ resources/views/components/doc/script.blade.php
@@ -7,13 +7,3 @@
 <script src="/assets/js/slick.min.js"></script>
 <script src="/assets/js/jquery.nice-select.js"></script>
 <script src="/assets/js/main.js"></script>
-
-
-<!-- Optional JavaScript; choose one of the two! -->
-    <script src="/assets/js/jquery.min.js"></script>
-    <script src="/assets/js/popper.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/owl.carousel.min.js"></script>
-    
-    <script src="/assets/js/jquery.nice-select.js"></script>
-    <script src="/assets/js/main.js"></script>

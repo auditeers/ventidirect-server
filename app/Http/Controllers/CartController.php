0c18c050e05760cafad8987d305487d2677e51c9
Maurice Lourens <maurice@auditeers.net>
2022-12-06 20:10:52 +0100

    inport validator

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {

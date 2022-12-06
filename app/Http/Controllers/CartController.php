505b6431741a8f45257fc2967f940761fad72d59
Maurice Lourens <maurice@auditeers.net>
2022-12-06 20:51:38 +0100

    customer class import

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Customer;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;

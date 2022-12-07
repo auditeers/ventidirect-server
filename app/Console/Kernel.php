f57414c4562355afe373812fd3aad9abd082542f
Maurice Lourens <maurice@auditeers.net>
2022-12-07 09:50:31 +0100

    typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->commant('venti:cleanorders')->hourly();
+        $schedule->command('venti:cleanorders')->hourly();
     }
 
     /**
